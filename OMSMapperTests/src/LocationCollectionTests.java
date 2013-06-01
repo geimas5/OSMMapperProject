5dca422e7a6345e81525614bd590d36783c24960
Marius Geitle <geipmas5@gmail.com>
2013-06-02 00:11:30 +0200

    Initial commit

---
+++ OMSMapperTests/src/LocationCollectionTests.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Marius Geitle on 31.05.13.
+ */
+public class LocationCollectionTests {
+
+    @Test
+    public void TestRemovedEntities(){
+
+    }
+}
